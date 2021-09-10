5b426373361614643dcb33fa144a505524e98586
huangjinbin <841421797@qq.com>
2021-09-10 15:58:06 +0800

    Optimize Fmeasure and add community increase based and modularity based metrics

--- htree.h
+++ htree.h
@@ -17,4 +17,36 @@ Node *newNode(int size)
     temp->size=size;
     return temp;
 }
+
+double communitySizeIncrease(Node *root)
+{
+    queue<Node *> q;
+    queue<Node *> nextQ;
+    q.push(root);
+    int level=0;
+    int sizeInc=0;
+    while(!q.empty())
+    {
+        Node *cur=q.front();
+        q.pop();
+        int maxSize=0;
+        for (int i = 0; i < cur->child.size(); ++i) {
+            nextQ.push(cur->child[i]);
+            if (maxSize<cur->child[i]->size)
+            {
+                maxSize=cur->child[i]->size;
+            }
+        }
+        sizeInc+=cur->size-maxSize;
+        if (q.empty())
+        {
+            q=nextQ;
+            level++;
+            nextQ=queue<Node *>();
+        }
+    }
+
+    double avgInc=(double)sizeInc/level;
+    return avgInc;
+}
 #endif //BOTTOM_UP_HTREE_H
